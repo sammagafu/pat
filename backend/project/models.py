e5318235bced226171d530cdf28a984130e03dca
Samuel Magafu <samuelmagafu@codex.local>
2022-08-30 16:05:33 +0300

    updated project model

--- backend/project/models.py
+++ backend/project/models.py
@@ -2,13 +2,19 @@ from django.db import models
 from django.utils.translation import ugettext_lazy as _
 
 # Create your models here.
+class ProjectDonor(models.Model):
+    donors = models.CharField(_("Donors Name"), max_length=150)
+    website = models.URLField(_("Donors Website"))
+    donors = models.ImageField(_("Donors Logo"), upload_to="donors/logo/")
+
 class Project(models.Model):
+    cover = models.ImageField(_("Project Cover Image"), upload_to="project/cover/")
     projectname = models.CharField(_("Project Name"), max_length=150)
+    donor = models.ForeignKey(ProjectDonor, verbose_name=_(""), on_delete=models.CASCADE)
     startdate = models.DateField(_("Starting Date"), auto_now=False, auto_now_add=False)
     shortdescription = models.TextField(_("Short Description"))
     specificObjective = models.TextField(_("Specific Objective"))
     enddate = models.DateField(_("End Date"), auto_now=False, auto_now_add=False)
-
 
     class Meta:
         verbose_name = _("Project")
