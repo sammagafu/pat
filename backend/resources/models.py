bdd20592b571fdd47787b0f162918840adf06381
Samuel Magafu <hi@sammagafu.com>
2022-09-05 12:20:31 +0300

    updated serializer and model

--- backend/resources/models.py
+++ backend/resources/models.py
@@ -2,12 +2,26 @@ from django.db import models
 from django.utils.translation import ugettext_lazy as _
 from django.utils.text import slugify
 
+class ResourceCategory(models.Model):
+    title = models.CharField(_("Category Name"), max_length=160)
+    slug = models.SlugField(_("Slug"),editable=False,unique=True,blank=False)
 
+    def __str__(self):
+        return self.title
+
+    def save(self):
+        if not self.slug:
+            self.slug = slugify(self.title)
+        self.slug = slugify(self.title)
+        super().save()
+
+    
 # Create your models here.\
 class Resource(models.Model):
     title = models.CharField(_("Document Name"), max_length=160)
     slug = models.SlugField(_("Slug"),editable=False,unique=True,blank=False)
     file = models.FileField(_("File Name"),upload_to="uploads/documents")
+    category = models.ManyToManyField(ResourceCategory, verbose_name=_("Product Category"))
     published = models.BooleanField(_("Published"),default=False)
     created = models.DateTimeField(_("Created at"), auto_now=True,)
     description = models.TextField(_("Descriptions"))
--- backend/resources/serializer.py
+++ backend/resources/serializer.py
@@ -1,7 +1,13 @@
+from dataclasses import field
 from rest_framework import serializers
-from .models import Resource
+from .models import Resource,ResourceCategory
+
+class ResourceCategory(serializers.ModelSerializer):
+    class Meta:
+        model = ResourceCategory
+        fields = ['title']
 
 class ResourceSerializer(serializers.ModelSerializer):
     class Meta:
         model = Resource
-        fields = ['id','title', 'file','description','published', 'published', 'created']
+        fields = ['id','title', 'file','description','published', 'category', 'created']
