8a0e9e24f585e41182ed7a374840ba9ba39231b5
Samuel Magafu <hi@sammagafu.com>
2022-09-30 01:45:45 +0300

    added image avatar

--- backend/membership/serializer.py
+++ backend/membership/serializer.py
@@ -7,12 +7,12 @@ from djoser.serializers import  UserCreateSerializer as BaseUserCreateSerializer
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta(BaseUserCreateSerializer.Meta):
-        fields = ('first_name','last_name',"middle_name",'email','phone','typeofmember','memberId','password','gender','region','organization','profession','areaofwork','typeofmember','mctnumber','avatar','collage','year')
+        fields = ('first_name','last_name',"middle_name",'email','phone','typeofmember','memberId','gender','region','organization','profession','areaofwork','typeofmember','mctnumber','avatar','collage','year')
         write_only_fields = ['password']
 
 class UserSerializerCreate(BaseUserCreateSerializer):
     class Meta(BaseUserCreateSerializer.Meta):
-        fields = ('first_name','last_name',"middle_name",'email','phone','typeofmember','memberId','password','gender','region','organization','profession','areaofwork','typeofmember','mctnumber','avatar','collage','year')
+        fields = ('first_name','last_name',"middle_name",'email','phone','typeofmember','memberId','gender','region','organization','profession','areaofwork','typeofmember','mctnumber','avatar','collage','year')
         read_only_fields = ['get_avatar']
         write_only_fields = ['password']
 
