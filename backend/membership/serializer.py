9880c2e09bf6522820cc92052c219c76093a6602
Samuel Magafu <hi@sammagafu.com>
2022-09-30 01:49:19 +0300

    avatar is working

--- backend/membership/serializer.py
+++ backend/membership/serializer.py
@@ -9,6 +9,7 @@ class UserSerializer(serializers.ModelSerializer):
     class Meta(BaseUserCreateSerializer.Meta):
         fields = ('first_name','last_name',"middle_name",'email','phone','typeofmember','memberId','gender','region','organization','profession','areaofwork','typeofmember','mctnumber','avatar','collage','year')
         write_only_fields = ['password']
+        read_only_fields = ['get_avatar']
 
 class UserSerializerCreate(BaseUserCreateSerializer):
     class Meta(BaseUserCreateSerializer.Meta):
