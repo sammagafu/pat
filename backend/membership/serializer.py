d83db092a1802ce953fdb2d233663f9e26d54c54
Samuel Magafu <hi@sammagafu.com>
2022-09-30 01:54:02 +0300

    avatar is working

--- backend/membership/serializer.py
+++ backend/membership/serializer.py
@@ -7,7 +7,7 @@ from djoser.serializers import  UserCreateSerializer as BaseUserCreateSerializer
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta(BaseUserCreateSerializer.Meta):
-        fields = ('first_name','last_name',"middle_name",'email','phone','typeofmember','memberId','gender','region','organization','profession','areaofwork','typeofmember','mctnumber','avatar','collage','year')
+        fields = ('first_name','last_name',"middle_name",'email','phone','typeofmember','memberId','gender','region','organization','profession','areaofwork','typeofmember','mctnumber','avatar','collage','year','get_avatar')
         write_only_fields = ['password']
         read_only_fields = ['get_avatar']
 
