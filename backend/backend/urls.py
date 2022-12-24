9c98d53e4444ceda64578a3ba2a66aec4379e1c7
bichwaa <ptonny13@gmail.com>
2022-12-24 14:13:32 +0300

    Added home view

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -2,8 +2,10 @@ from django.contrib import admin
 from django.urls import path,include
 from django.conf import settings
 from django.conf.urls.static import static
+from .views import home_view
 
 urlpatterns = [
+    path('', home_view),
     path('admin/', admin.site.urls),
     path('api/v1/resource/',include('resources.urls')),
     path('api/v1/project/',include('project.urls')),
